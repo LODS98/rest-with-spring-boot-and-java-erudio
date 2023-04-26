6b143b39c4b960aa982ed43ad9e55cc144f7d8b9
Lucas Oliveira <lucasmct23@gmail.com>
2023-04-26 17:41:42 -0300

    Adicionando Migrations com Flyway.

---
+++ 05_FirstStepsinJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V1__Create_Table_Person.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS `person` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(80) NOT NULL,
+  `last_name` varchar(80) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `gender` varchar(6) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Rio De Janeiro', 'Lucas', 'Male', 'Oliveira'),
+	(2, 'Duque de Caxias - Rio de Janeiro - Brasil', 'Leonardo', 'Male', 'Da vinci'),
+	(4, 'India', 'Indira', 'Female', 'Gandhi'),
+	(5, 'India', 'Indira', 'Female', 'Gandhi'),
+	(6, 'Kentucky - US', 'Muhammad', 'Male', 'Ali'),
+	(7, 'Mvezo - South Africa', 'Nelson', 'Male', 'Mandela'),
+	(8, 'Smiljan - Croatia', 'Nikola', 'Male', 'Tesla');
---
+++ 05_FirstStepsinJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,8 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Rio De Janeiro', 'Lucas', 'Male', 'Oliveira'),
+	(2, 'Duque de Caxias - Rio de Janeiro - Brasil', 'Leonardo', 'Male', 'Da vinci'),
+	(4, 'India', 'Indira', 'Female', 'Gandhi'),
+	(5, 'India', 'Indira', 'Female', 'Gandhi'),
+	(6, 'Kentucky - US', 'Muhammad', 'Male', 'Ali'),
+	(7, 'Mvezo - South Africa', 'Nelson', 'Male', 'Mandela'),
+	(8, 'Smiljan - Croatia', 'Nikola', 'Male', 'Tesla');
