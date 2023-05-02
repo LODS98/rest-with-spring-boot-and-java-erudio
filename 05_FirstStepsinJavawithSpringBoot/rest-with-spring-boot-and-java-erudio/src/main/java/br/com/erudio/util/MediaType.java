2359611d902a51897da6b2c7fa8e0b6eb1cbc142
Lucas Oliveira <lucasmct23@gmail.com>
2023-05-02 17:32:26 -0300

    Implementando Content Negotiation

---
+++ 05_FirstStepsinJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/util/MediaType.java
@@ -0,0 +1,10 @@
+package br.com.erudio.util;
+
+public class MediaType {
+	
+	public static final String APPLICATION_JSON = "Application/json";
+	public static final String APPLICATION_XML = "Application/xml";
+	public static final String APPLICATION_YML = "Application/x-yaml";
+	
+
+}
